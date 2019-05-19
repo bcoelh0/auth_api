42b5002446cf29ff442811b4a66e37d0cd2598c6
Bruno Coelho <brunocsncoelho@gmail.com>
2019-05-19 16:52:34 +0100

    updates cors host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://authorization-app-react.herokuapp.com'
+    origins 'https://authentication-app-react.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
