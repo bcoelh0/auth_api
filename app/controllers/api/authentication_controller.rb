f245a09be353023e7e5c78a1ee4e745432b91d29
Bruno Coelho <brunocsncoelho@gmail.com>
2019-05-19 14:12:27 +0100

    adds cors compatibility

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -4,7 +4,7 @@ class Api::AuthenticationController < ApplicationController
   # POST /login
   def login
     @user = User.find_by_email(params[:email])
-    if @user.authenticate(params[:password])
+    if @user.present? && @user.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
